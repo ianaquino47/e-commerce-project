bd7e7c2eec35d714c129a988475845fcfd730136
iAquino47 <aquinoian@icloud.com>
2020-05-11 16:58:16 +0100

    simple landing page with lorem ipsum text

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => ( 
+    <div>
+        <h2>Hello World</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean faucibus mauris vel leo rutrum, sed accumsan ipsum sodales. Praesent sagittis gravida dictum. Nam vitae magna eget turpis pretium scelerisque a blandit felis. Nunc tempus, orci sed tincidunt vulputate, ipsum ligula feugiat nisi, ac iaculis metus sem eget magna. Nam laoreet ornare mi eu porta. Proin vehicula arcu lobortis volutpat pulvinar. Vivamus tincidunt erat ligula, vel mattis dolor convallis eget. Praesent pulvinar fermentum turpis. Phasellus congue nibh id orci lobortis luctus. Vestibulum eu accumsan dui, aliquet vehicula tellus. Suspendisse sagittis arcu eros, ac vulputate ante tempor id.</p>
+        <p>Sed nec tortor malesuada, vehicula massa quis, dictum urna. Donec at sem eu nisi semper mattis vel sed est. Vestibulum gravida vestibulum pellentesque. Donec sodales tempus purus, pulvinar auctor lorem dignissim vitae. Sed sit amet dapibus dolor, eu posuere tellus. Aliquam tincidunt luctus metus, vel pellentesque nisl commodo sed. Nullam tristique ligula vel mauris elementum, nec tempor nisi dapibus. Duis venenatis vestibulum nibh, vulputate bibendum mi tempus sed. Donec pellentesque ipsum sit amet erat varius, in interdum felis cursus. Aliquam ac ex ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In condimentum sagittis sem, ac finibus nunc rhoncus sit amet. Sed vitae dictum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus tellus ut ante sodales, nec faucibus mi aliquet. Nullam venenatis, leo at consequat hendrerit, orci tortor accumsan tellus, eget efficitur quam ipsum at velit.</p>
+    </div>
+)
+
